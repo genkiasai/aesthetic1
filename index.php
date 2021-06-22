edc7e2e884f51462ea1f3c87f9f64e6dd41d8d37
genkiasai <baseballboy1025@gmail.com>
2021-06-23 05:25:35 +0900

    20210622_トップページヘッダー作成開始

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>@TODO</title>
+</head>
+<body>
+    <header>
+        <!-- 会社名 -->
+        <nav>
+            <div class="row">
+                <div class="col-xl-4 header_contents header_contents-1"><a class="header header_company-name" href="./">@TODO会社名</a></div>
+                <div class="col-xl-2 header_contents header_contents-2">
+                    <a class="header header_menu header_menu-1" href="./">
+                        <div class="header_menu-1-1">HOME</div>
+                        <div class="header_menu-1-2">ホーム</div>
+                    </a>
+                </div>
+                <div class="col-xl-2 header_contents header_contents-2">
+                    <a class="header header_menu header_menu-1" href="./">
+                        <div class="header_menu-2-1">THERAPIST</div>
+                        <div class="header_menu-2-2">セラピスト</div>
+                    </a>
+                </div>
+                <div class="col-xl-2 header_contents header_contents-2">
+                    <a class="header header_menu header_menu-1" href="./">
+                        <div class="header_menu-3-1">MENU</div>
+                        <div class="header_menu-3-2">メニュー</div>
+                    </a>
+                </div>
+                <div class="col-xl-2 header_contents header_contents-2">
+                    <a class="header header_menu header_menu-1" href="./">
+                        <div class="header_menu-4-1">ACCESS</div>
+                        <div class="header_menu-4-2">アクセス</div>
+                    </a>
+                </div>
+            </div>
+        </nav>
+    </header>
+    
+    <main>
+    
+    </main>
+
+    <footer>
+    
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>

227d96146940313205a25b74d5c10ba6b63c0ca3
genkiasai <baseballboy1025@gmail.com>
2021-06-23 07:33:18 +0900

    20210623_wrapper-3途中

--- index.php
+++ index.php
@@ -13,29 +13,29 @@
         <!-- 会社名 -->
         <nav>
             <div class="row">
-                <div class="col-xl-4 header_contents header_contents-1"><a class="header header_company-name" href="./">@TODO会社名</a></div>
-                <div class="col-xl-2 header_contents header_contents-2">
-                    <a class="header header_menu header_menu-1" href="./">
-                        <div class="header_menu-1-1">HOME</div>
-                        <div class="header_menu-1-2">ホーム</div>
+                <div class="col-xl-4 header__contents header__contents-1"><a class="header header__company-name" href="./">@TODO会社名</a></div>
+                <div class="col-xl-2 header__contents header__contents-2">
+                    <a class="header header__menu header__menu-1" href="./">
+                        <div class="header__menu-1-1">HOME</div>
+                        <div class="header__menu-1-2">ホーム</div>
                     </a>
                 </div>
-                <div class="col-xl-2 header_contents header_contents-2">
-                    <a class="header header_menu header_menu-1" href="./">
-                        <div class="header_menu-2-1">THERAPIST</div>
-                        <div class="header_menu-2-2">セラピスト</div>
+                <div class="col-xl-2 header__contents header__contents-2">
+                    <a class="header header__menu header__menu-1" href="./">
+                        <div class="header__menu-2-1">THERAPIST</div>
+                        <div class="header__menu-2-2">セラピスト</div>
                     </a>
                 </div>
-                <div class="col-xl-2 header_contents header_contents-2">
-                    <a class="header header_menu header_menu-1" href="./">
-                        <div class="header_menu-3-1">MENU</div>
-                        <div class="header_menu-3-2">メニュー</div>
+                <div class="col-xl-2 header__contents header__contents-2">
+                    <a class="header header__menu header__menu-1" href="./">
+                        <div class="header__menu-3-1">MENU</div>
+                        <div class="header__menu-3-2">メニュー</div>
                     </a>
                 </div>
-                <div class="col-xl-2 header_contents header_contents-2">
-                    <a class="header header_menu header_menu-1" href="./">
-                        <div class="header_menu-4-1">ACCESS</div>
-                        <div class="header_menu-4-2">アクセス</div>
+                <div class="col-xl-2 header__contents header__contents-2">
+                    <a class="header header__menu header__menu-1" href="./">
+                        <div class="header__menu-4-1">ACCESS</div>
+                        <div class="header__menu-4-2">アクセス</div>
                     </a>
                 </div>
             </div>
@@ -43,7 +43,84 @@
     </header>
     
     <main>
-    
+        <!-- wrapper-1 -->
+        <div class="wrapper wrapper-1">
+            <h1 class="wrapper-1__catch-copy">キャッチコピー</h1>
+        </div>
+        <!-- /wrapper-1 -->
+
+        <!-- wrapper-2 -->
+        <div class="wrapper wrapper-2">
+            <div class="inner inner-2">
+                <h2 class="wrapper__h2">インフォメーション</h2>
+                <div class="info-area">
+                    <div class="row">
+                        <div class="col-xl-12 info info-1">
+                            <div class="float-left info-data date date-1">2021/6/15</div>
+                            <div class="float-right info-data sentence sentence-1">Webサイトが完成しました！</div>
+                        </div>
+                        <div class="col-xl-12 info info-2">
+                            <div class="float-left info-data date date-2">2021/6/15</div>
+                            <div class="float-right info-data sentence sentence-2">Webサイトが完成しました！</div>
+                        </div>
+                        <div class="col-xl-12 info info-3">
+                            <div class="float-left info-data date date-3">2021/6/15</div>
+                            <div class="float-right info-data sentence sentence-3">Webサイトが完成しました！</div>
+                        </div>
+                        <div class="col-xl-12 info info-4">
+                            <div class="float-left info-data date date-4">2021/6/15</div>
+                            <div class="float-right info-data sentence sentence-4">Webサイトが完成しました！</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /wrapper-2 -->
+
+        <!-- wrapper-3 -->
+        <div class="wrapper wrapper-3">
+            <div class="inner inner-3">
+                <h2 class="wrapper__h2">セラピスト</h2>
+                <div class="images-area">
+                    <div class="image-area image-area-1"><img class="image" src="./images/genki.png" alt=""></div>
+                    <div class="image-area image-area-2"><img class="image" src="./images/genki.png" alt=""></div>
+                    <div class="image-area image-area-3"><img class="image" src="./images/genki.png" alt=""></div>
+                </div>
+            </div>
+        </div>
+        <!-- /wrapper-3 -->
+
+        <!-- wrapper-4 -->
+        <div class="wrapper wrapper-4">
+            <div class="inner inner-4">
+                
+            </div>
+        </div>
+        <!-- /wrapper-4 -->
+
+        <!-- wrapper-5 -->
+        <div class="wrapper wrapper-5">
+            <div class="inner inner-5">
+                
+            </div>
+        </div>
+        <!-- /wrapper-5 -->
+
+        <!-- wrapper-6 -->
+        <div class="wrapper wrapper-6">
+            <div class="inner inner-6">
+                
+            </div>
+        </div>
+        <!-- /wrapper-6 -->
+
+        <!-- wrapper-7 -->
+        <div class="wrapper wrapper-7">
+            <div class="inner inner-7">
+                
+            </div>
+        </div>
+        <!-- /wrapper-7 -->
     </main>
 
     <footer>

5f2466400677a581dc281f72dc8dbb09c7868adc
genkiasai <baseballboy1025@gmail.com>
2021-06-23 23:07:15 +0900

    20210623_トップページ大枠完成

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
         <!-- 会社名 -->
         <nav>
             <div class="row">
-                <div class="col-xl-4 header__contents header__contents-1"><a class="header header__company-name" href="./">@TODO会社名</a></div>
+                <div class="col-xl-4 header__contents header__contents-1"><a class="header header__company-name" href="./">@TODO店名</a></div>
                 <div class="col-xl-2 header__contents header__contents-2">
                     <a class="header header__menu header__menu-1" href="./">
                         <div class="header__menu-1-1">HOME</div>
@@ -81,11 +81,23 @@
         <div class="wrapper wrapper-3">
             <div class="inner inner-3">
                 <h2 class="wrapper__h2">セラピスト</h2>
-                <div class="images-area">
-                    <div class="image-area image-area-1"><img class="image" src="./images/genki.png" alt=""></div>
-                    <div class="image-area image-area-2"><img class="image" src="./images/genki.png" alt=""></div>
-                    <div class="image-area image-area-3"><img class="image" src="./images/genki.png" alt=""></div>
+                <div class="images-area images-area-1">
+                    <!-- <div class="arrow-area arrow-area-1"><a href="./"><img class="arrow-previous" src="./images/arrow-previous.png" alt=""></a></div> -->
+                    <div class="image-area image-area-1"><a class="therapist" href="./"><img class="image" src="./images/genki.png" alt=""><div class="intro intro-1"><p class="no-margin">自己紹介</p><p class="no-margin">自己紹介</p></div></a></div>
+                    <div class="image-area image-area-2"><a class="therapist" href="./"><img class="image" src="./images/genki.png" alt=""><div class="intro intro-1"><p class="no-margin">自己紹介</p><p class="no-margin">自己紹介</p></div></a></div>
+                    <div class="image-area image-area-3"><a class="therapist" href="./"><img class="image" src="./images/genki.png" alt=""><div class="intro intro-1"><p class="no-margin">自己紹介</p><p class="no-margin">自己紹介</p></div></a></div>
+                    <div class="image-area image-area-4"><a class="therapist" href="./"><img class="image" src="./images/genki.png" alt=""><div class="intro intro-1"><p class="no-margin">自己紹介</p><p class="no-margin">自己紹介</p></div></a></div>
+                    <!-- <div class="arrow-area arrow-area-2"><a href="./"><img class="arrow-previous" src="./images/arrow-next.png" alt=""></a></div> -->
                 </div>
+                <div class="images-area images-area-2">
+                    <!-- <div class="arrow-area arrow-area-1"><a href="./"><img class="arrow-previous" src="./images/arrow-previous.png" alt=""></a></div> -->
+                    <div class="image-area image-area-1"><a class="therapist" href="./"><img class="image" src="./images/genki.png" alt=""><div class="intro intro-1"><p class="no-margin">自己紹介</p><p class="no-margin">自己紹介</p></div></a></div>
+                    <div class="image-area image-area-2"><a class="therapist" href="./"><img class="image" src="./images/genki.png" alt=""><div class="intro intro-1"><p class="no-margin">自己紹介</p><p class="no-margin">自己紹介</p></div></a></div>
+                    <div class="image-area image-area-3"><a class="therapist" href="./"><img class="image" src="./images/genki.png" alt=""><div class="intro intro-1"><p class="no-margin">自己紹介</p><p class="no-margin">自己紹介</p></div></a></div>
+                    <div class="image-area image-area-4"><a class="therapist" href="./"><img class="image" src="./images/genki.png" alt=""><div class="intro intro-1"><p class="no-margin">自己紹介</p><p class="no-margin">自己紹介</p></div></a></div>
+                    <!-- <div class="arrow-area arrow-area-2"><a href="./"><img class="arrow-previous" src="./images/arrow-next.png" alt=""></a></div> -->
+                </div>
+                <a class="view-more" href="./">View More</a>
             </div>
         </div>
         <!-- /wrapper-3 -->
@@ -93,7 +105,15 @@
         <!-- wrapper-4 -->
         <div class="wrapper wrapper-4">
             <div class="inner inner-4">
-                
+                <div class="row">
+                    <div class="col-xl-12">
+                        <div class="message-area">
+                            <div class="message message-1">メッセージメッセージメッセージメッセージメッセージ</div>
+                            <div class="message message-2">メッセージメッセージメッセージメッセージメッセージメッセージメッセージメッセージメッセージメッセージメッセージメッセージメッセージメッセージメッセージメッセージ</div>
+                            <div class="message message-3">メッセージメッセージメッセージメッセージメッセージメッセージメッセージメッセージメッセージメッセージ</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         <!-- /wrapper-4 -->
@@ -101,7 +121,9 @@
         <!-- wrapper-5 -->
         <div class="wrapper wrapper-5">
             <div class="inner inner-5">
-                
+                <div class="menu">
+                    <h2 class="wrapper__h2">メニュー</h2>
+                </div>
             </div>
         </div>
         <!-- /wrapper-5 -->
@@ -109,7 +131,27 @@
         <!-- wrapper-6 -->
         <div class="wrapper wrapper-6">
             <div class="inner inner-6">
-                
+                <h2 class="wrapper__h2">アクセス</h2>
+                <div class="access-area">
+                    <div class="row">
+                        <div class="col-xl-12 access access-1">
+                            <div class="access-item access-address address">場所</div>
+                            <div class="float-right access-address sentence sentence-address">名鉄岐阜駅から西に徒歩5分程度（ご希望があればお迎えに上がります）</div>
+                        </div>
+                        <div class="col-xl-12 access access-2">
+                            <div class="access-item access-tel tel">TEL</div>
+                            <div class="float-right access-tel sentence sentence-tel">000-0000-0000</div>
+                        </div>
+                        <div class="col-xl-12 access access-3">
+                            <div class="access-item access-time time">営業時間</div>
+                            <div class="float-right access-time sentence sentence-time">14:00 - 24:00</div>
+                        </div>
+                        <div class="col-xl-12 access access-4">
+                            <div class="access-item access-holiday holiday">定休日</div>
+                            <div class="float-right access-holiday sentence sentence-holiday">年中無休</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         <!-- /wrapper-6 -->
@@ -117,7 +159,28 @@
         <!-- wrapper-7 -->
         <div class="wrapper wrapper-7">
             <div class="inner inner-7">
-                
+                <div class="caution">
+                    ▼△▼注意事項▼△▼<br>
+                    セラピストの引き抜き行為、スカウトは罰金50万円とします。<br>
+                    当店は本格的なアロマセラピーをご提供するサロンであり、風俗的サービスは一切行っておりません。<br>
+                    施術中にそのようなサービスの要求、品性の無い発言・行動をされた場合またはセラピストが施術続行不可能と判断した場合には、直ちに施術を中止し、ご退店をしていただく事が御座います。<br>
+                    その際の料金の返金は一切いたしませんので予めご了承ください。<br>
+                    <br>
+                    また、下記項目に該当する方の、当店のご利用は控えさせていただいております。<br>
+                    予めご了承いただきますようお願い申し上げます。<br>
+                    ・風邪などで発熱のある方<br>
+                    ・泥酔状態の方<br>
+                    ・重度の皮膚疾患がある方（皮膚炎・水虫など）<br>
+                    ・セラピストの体に触るなど性的なサービスの要求、暴言・セラピストの嫌がる行為をされる方<br>
+                    ・スカウトや引き抜き行為をされる方<br>
+                    ・盗撮、盗聴などの行為をする方<br>
+                    ・暴力団関係者又はそれに準ずる方、刺青・タトゥーのある方<br>
+                    ・当店運営、営業に支障をきたす方<br>
+                    ・当店の趣旨をご理解頂けない方、当店がふさわしくないと判断した方<br>
+                    ・これらの事は、当サロンを快適・安全にご利用いただくために必要な事ですので、何卒ご理解の程をお願い申し上げます。また、施術中に発覚した場合は施術中でもサービス終了、即退店して頂きます。<br>
+                    ・その場合、料金の返金は一切いたしませんので、予めご了承いただけますようお願い申し上げます。<br>
+                    ・当店は「医療法」が定める「病院、診療所、治療院」や、あん摩、マッサージ指圧師、鍼灸師等が定める施術所では御座いません。
+                </div>
             </div>
         </div>
         <!-- /wrapper-7 -->
